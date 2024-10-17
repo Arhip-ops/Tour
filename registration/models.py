080d4d95b9ef3d970baca9025199031b7a007895
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2024-10-17 20:12:51 +0300

    tour

--- registration/models.py
+++ registration/models.py
@@ -8,3 +8,11 @@ class User(DATABASE.Model, flask_login.UserMixin):
     password = DATABASE.Column(DATABASE.String(256), nullable = False)
     def __repr__(self):
         return f"Ім'я {self.name} {self.password}"
+
+class Tour(DATABASE.Model):
+    id = DATABASE.Column(DATABASE.Integer, primary_key = True)
+    title = DATABASE.Column(DATABASE.String(256), nullable = False)
+    date = DATABASE.Column(DATABASE.String(10), nullable = False)
+    country = DATABASE.Column(DATABASE.String(100), nullable = False)    
+    price = DATABASE.Column(DATABASE.Integer, nullable = False)
+    description = DATABASE.Column(DATABASE.String(500), nullable = False)
