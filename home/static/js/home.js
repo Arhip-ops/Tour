0f5a0180cb859f0cf9256fb11be3c13f1ec39363
Arhip_ops <harkhip777@gmail.com>
2024-10-24 20:48:35 +0300

    Created modal for email

---
+++ home/static/js/home.js
@@ -0,0 +1,10 @@
+const buttonSend = document.querySelector(".send")
+
+buttonSend.addEventListener(
+    type = "click",
+    listener = (event) => {
+        document.querySelector(".emailDiv").style.display = "flex"
+        console.log("12312")
+    }
+
+)
