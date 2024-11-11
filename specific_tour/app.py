82ce7374041baf1a1e3b2b97ce3aeb5ce7303611
Artem <artem.vashenko201010@gmail.com>
2024-11-11 20:42:52 +0200

    Created design for page all tours and page specific tour

--- specific_tour/app.py
+++ specific_tour/app.py
@@ -3,5 +3,7 @@ import flask
 specific_tour_app = flask.Blueprint(
     name="specific_tour_app",
     import_name="specific_tour",
-    template_folder="templates"
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/specific_tour/"
 )
